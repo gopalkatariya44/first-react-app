666bed0381dc2b1d4bc3958abf22c95fe6342180
Gopal Katariya <katariyagopal60@gmail.com>
2023-07-30 17:05:02 +0530

    init

--- src/App.js
+++ src/App.js
@@ -4,27 +4,8 @@ import Axios from 'axios';
 
 function App() {
 
-  const [name, setName] = useState("");
-  const [age, setAge] = useState(null);
-
-  const fetchData = () => {
-    Axios.get(`https://api.agify.io/?name=${name}`).then((res) => {
-      console.log(res.data);
-      setAge(res.data);
-    });
-  };
-
   return (
-    <div className="App">
-      <input placeholder='Ex. Gopal...' onChange={(event) => {
-        setName(event.target.value)
-      }} />
-      &nbsp;
-      <button onClick={fetchData}>Predict Age</button>
-      <h1>Name: {age?.name}</h1>
-      <h1>Predicted Age: {age?.age}</h1>
-      <h1>Count: {age?.count}</h1>
-    </div>
+    <div className="App"></div>
   );
 }
 export default App;
