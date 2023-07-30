4b0777a15fac762ff6d325659f762f237a43da43
Gopal Katariya <katariyagopal60@gmail.com>
2023-07-30 16:15:42 +0530

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,53 +1,19 @@
 import './App.css';
 import { useState } from 'react';
-import Task, { } from './Task';
+import Text, { } from './Text';
 
 function App() {
-  const [todoList, setTodoList] = useState([])
-  const [newTask, setNewTask] = useState("")
-
-  function handleChange(event) {
-    setNewTask(event.target.value);
-
-  }
-  function addTask() {
-    const task = {
-      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
-      taskName: newTask,
-      done: false,
-    };
-    setTodoList([...todoList, task]);
-    console.log(todoList);
-  }
-
-  function deleteTask(id) {
-    setTodoList(todoList.filter((task) => task.id !== id));
-  }
-  function doneTask(id) {
-    setTodoList(todoList.map((task) => {
-      if (task.id === id) {
-        return { ...task, done: true };
-      } else {
-        return task
-      }
-    }));
-  }
+  const [showText, setShowText] = useState(false);
 
   return (
     <div className="App">
-      <div className='addTask'>
-        <input onChange={handleChange} type='text' />&nbsp;
-        <button onClick={addTask}>Add Task</button>
-      </div>
 
-      <div className='list'>
-        {
-          todoList.map((task) => {
-            return <Task done={task.done} taskName={task.taskName} id={task.id} deleteTask={deleteTask} doneTask={doneTask} />
-          })
-        }
-      </div>
-
+      <button onClick={() => { setShowText(!showText) }}>
+        Show Text
+      </button>
+      <br />
+      <br />
+      {showText && <Text />}
     </div>
   );
 }
